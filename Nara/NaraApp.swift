57cc9c4d5e3ed3593f6377d1249989cd8274f09e
Eylül Deniz <145336470+eyluldenizs@users.noreply.github.com>
2026-05-20 13:21:53 +0300

    Initial Commit

---
+++ Nara/NaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaraApp.swift
+//  Nara
+//
+//  Created by Eylül Soylu on 20.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct NaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
